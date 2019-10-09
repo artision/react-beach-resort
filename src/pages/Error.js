7e8388187f466e3ae26b9d0c6de37ef2e18109f7
artision <art.2413@hotmail.com>
2019-10-09 23:01:09 +0200

    added routes

---
+++ src/pages/Error.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Error() {
+  return <div>Heloo from error page</div>;
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div>Heloo from home page</div>;
+}
